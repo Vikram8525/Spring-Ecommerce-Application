e4836ae6d2d6e108b42cf64750a34c17d44016b6
thir3575 <thir3575@CSL0564.global.chainsys.com>
2024-06-28 18:08:51 +0530

    update

---
+++ ecomwebapplication/src/main/java/com/chainsys/ecomwebapplication/EcomwebapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.chainsys.ecomwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomwebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomwebapplicationApplication.class, args);
+	}
+
+}
---
+++ ecomwebapplication/src/test/java/com/chainsys/ecomwebapplication/EcomwebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chainsys.ecomwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomwebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
